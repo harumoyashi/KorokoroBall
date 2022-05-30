ab7c551031dd43d04e1d9b0ec02c7c2a25444975
晴彦 相川 <k021g1126@g.neec.ac.jp>
2022-05-31 01:58:28 +0900

    第6回完了(ビルドまで)

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameManager gameManager;
     void Start()
     {
-
+        //まずは「ゲームオブジェクト」を探して変数に控えておく
+        GameObject managerObject = GameObject.Find("GameManager");
+        //控えておいたゲームオブジェクトに対してGetConponentでGameManagerコンポーネントを取得
+        gameManager=managerObject.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -16,10 +20,11 @@ public class Coin : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter(Collider other) //other:�Փ˂��Ă�������̏��
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == ("Player"))
         {
+            gameManager.AddCoinCount(); //コインの取得数カウント
             Destroy(gameObject);
         }
 
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,29 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;  //忘れない
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Screen.SetResolution(1920, 1080, false);    //1920x1080のウィンドウモード
+                                                    //trueにするとフルスクリーンになる
+        Application.targetFrameRate = 60;   //FPS固定
+        coinCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    //リセットボタン
     public void SceneReset()
     {
         string activeSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(activeSceneName);
     }
 
+    //次のシーンに切り替え
     public void ChangeScene(string nextScene)
     {
         SceneManager.LoadScene(nextScene);
+    }
+
+    //コイン取得数カウント
+    private int coinCount;
+    public Text textComponent;
+    public void AddCoinCount()
+    {
+        coinCount++;
+        Debug.Log("CoinCount:" + coinCount);
+        textComponent.text = "CoinCount:" + coinCount;
     }
 }
